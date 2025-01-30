3fae87ee7a85dce0d8d641c63169b41ed17fa4b8
Ivan Niveyro <iiniveyro@gmail.com>
2025-01-30 09:17:53 -0300

    Added more context classes

--- models/model.cs
+++ models/model.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 public class Ticket
 {
@@ -6,4 +7,25 @@ public class Ticket
     public int NumId { get; set; }
     public string? Description { get; set; }
     public bool Status { get; set; }
+    public required string Priority {get; set;}
+    //Foreign Keys
+    [ForeignKey ("NumInv")]
+    public required PC NumInv {get; set;}
+    [ForeignKey ("Name")]
+    public required Area Name {get; set;}
+
+}
+
+public class PC
+{
+    [Key]
+    public int NumInv {get; set;}
+    public int? NumSer {get; set;}
+    public string? Description {get;set;}
+}
+
+public class Area
+{
+    [Key]
+    public required string Name {get; set;}
 }
--- models/modelsDB.cs
+++ models/modelsDB.cs
@@ -5,4 +5,6 @@ class TicketsDb : DbContext
     public TicketsDb(DbContextOptions<TicketsDb> options)
         : base(options) { }
     public DbSet<Ticket> Tickets => Set<Ticket>();
+    public DbSet<PC> PC => Set<PC>();
+    public DbSet<Area> Area => Set<Area>();
 }

6af4d9d00939a6c074fd7ef1f57d3dacfc7d253b
iniveyro <iiniveyro@gmail.com>
2025-02-17 00:28:05 -0300

    Se modifico el tipo del campo Priority

--- models/Ticket.cs
+++ models/Ticket.cs
@@ -9,7 +9,13 @@ public class Ticket
     public int NumId { get; set; }
     public string? Description { get; set; }
     public bool Status { get; set; }
-    public required string Priority {get; set;}
+    public required PriorityLevel Priority {get; set;}
     public required Pc Pc {get; set;}
     public required Area Area {get; set;}
 }
+    public enum PriorityLevel
+    {
+        Urgente,
+        Moderada,
+        Baja
+    }
